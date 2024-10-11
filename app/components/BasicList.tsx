574b0b9a6a7d8549c03b3a214d06b602bf428104
Artur <a.shevchenko1+copilot@elsevier.com>
2024-10-11 22:24:46 +0300

    update ui

---
+++ app/components/BasicList.tsx
@@ -0,0 +1,22 @@
+import * as React from 'react';
+import Box from '@mui/material/Box';
+import List from '@mui/material/List';
+import ListItem from '@mui/material/ListItem';
+import ListItemButton from '@mui/material/ListItemButton';
+import ListItemText from '@mui/material/ListItemText';
+
+export default function BasicList() {
+  return (
+    <Box sx={{ width: '100%', maxWidth: 360, bgcolor: 'background.paper' }}>
+      <nav aria-label="main mailbox folders">
+        <List>
+          <ListItem disablePadding>
+            <ListItemButton>
+              <ListItemText primary="Введение" />
+            </ListItemButton>
+          </ListItem>
+        </List>
+      </nav>
+    </Box>
+  );
+}
---
+++ app/home/page.tsx
@@ -0,0 +1,11 @@
+import Header from '@/app/components/Header';
+import BasicTabs from '@/app/components/BasicTabs';
+
+export default function Home() {
+  return (
+    <>
+      <Header />
+      <BasicTabs />
+    </>
+  );
+}
